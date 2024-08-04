7795b01e92bd03d75585ae336a84adf4e86e1e9e
Halvadiya-Kalp <halvadiyakalp123@gmail.com>
2024-08-05 00:04:30 +0530

    Update task1.py

--- Task1/task1.py
+++ Task1/task1.py
@@ -1,3 +1,8 @@
+# Implement Caesar Cipher
+
+# Create a Python program that can encrypt and decrypt text using the Caesar Cipher algorithm. Allow users to input a message and a shift value to perform encryption and decryption.
+
+
 def caesar_cipher_encrypt(text, shift):
     encrypted_text = ""
     for char in text:
